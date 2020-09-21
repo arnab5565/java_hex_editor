f307250a17b79be1529e83ef8b2425d9e425b84a
arnab5565 <62198084+arnab5565@users.noreply.github.com>
2020-09-21 15:07:32 +0530

    Create Template.java

---
+++ Template.java
@@ -0,0 +1,11 @@
+package amtecnologies;
+
+import java.awt.image.BufferedImage;
+
+public interface Template{
+    
+    public abstract BufferedImage processImg(BufferedImage image);
+        
+        
+    
+}
